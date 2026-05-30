9681c1fe9601298fe785ca813e8de62637418303
Jithya <jithyakumaran.l@gmail.com>
2026-05-30 22:20:43 +0530

    day1_Palindrome_number solution

--- day1_Palindrome_number.py
+++ day1_Palindrome_number.py
@@ -1,3 +1,6 @@
+#1.Palindrome number
+#Given an integer x, return true if x is a palindrome, and false otherwise.
+
 def isPalindrome(self, x: int) -> bool:
         if (str(x)==str(x)[::-1]):
             return True
