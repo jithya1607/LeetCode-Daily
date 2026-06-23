716674ec278e67c83cfeeca9e5348f697bfd294d
Jithya <jithyakumaran.l@gmail.com>
2026-06-23 19:49:40 +0530

    day16_Fibonacci_number solution

---
+++ day16_Fibonacci_number.py
@@ -0,0 +1,9 @@
+#16 Fibonacci number
+# Return the nth Fibonacci number
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
