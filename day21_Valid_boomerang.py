b6ae32f8aa1e5425a0b4d1746f112075db34f08c
Jithya <jithyakumaran.l@gmail.com>
2026-06-28 03:25:31 +0530

    day21_Valid_boomerang solution

--- day21_Valid_boomerang.py
+++ day21_Valid_boomerang.py
@@ -1,6 +1,6 @@
 #21 Valid Boomerang
 # Given three points in the plane, determine if they form a valid boomerang (i.e., they are not collinear and not identical).
-def isBoomerang(self, points: List[List[int]]) -> bool:
+def isBoomerang(points):
     a=points[0][0]*(points[1][1]-points[2][1])
     b=points[0][1]*(points[1][0]-points[2][0])
     c=(points[1][0]*points[2][1])-(points[2][0]*points[1][1])
